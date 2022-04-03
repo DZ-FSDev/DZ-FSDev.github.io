2edbf2148182debafe8d57c0820d0aa2fcbdda77
DZ-FSDev <dz.uofm@gmail.com>
2022-04-03 12:37:27 -0500

    Update newsLetter.js

--- newsLetter.js
+++ newsLetter.js
@@ -1,13 +1,20 @@
 $().ready(() => {
     $('#newsLetterSubmit').on('click', e => {
         // #newsLetterForm
-        if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test($('#newsLetterForm').val())){
+        if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test($('#newsLetterForm').val())) {
             $('#newsLetterLabel').text('Thank you for subscribing!');
-        }else{
+            $('#newsLetterForm').val('');
+        } else {
             $('#newsLetterLabel').text('Email address is invalid.');
         }
-        
+
         // Sorry, not taking any subscriptions at the moment :)
         e.preventDefault();
-    })
+    });
+
+    $('#newsLetterForm').on('input',
+        e => {
+            $('#newsLetterLabel').text('Email address');
+        }
+    );
 });
