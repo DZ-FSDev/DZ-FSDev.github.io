8114b2a9cc62e5a43ed62e50f43c2a4fb118abcd
DZ-FSDev <dz.uofm@gmail.com>
2022-04-04 22:27:58 -0500

    Update contact.js

--- contact.js
+++ contact.js
@@ -5,6 +5,28 @@ $().ready(() => {
     }
 
     $('#contacter').on('submit',
-        ()=>console.log('hi')
+        () => {
+            let formValidators = {
+                name: /^.{1,100}$/,
+                phone: /^.{1,100}$/,
+                email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                message: /^.{1,255}$/,
+            };
+
+            for (let key in formValidators) {
+                let acceptable = formValidators[key].test(
+                    // Test n' Trim! (User shouldn't be able to submit spaces as a name.)
+                    $(`#${key}`).val($(`#${key}`).val().trim()).val()
+                );
+                $(`.contact-${key}-error`).css({ 'display': acceptable ? 'none' : 'inline' });
+                if (!acceptable && !hasErrors) {
+                    document.getElementById(`${key}`).focus();
+                }
+                $hasErrors ||= !$acceptable;
+            }
+
+
+        }
+
     )
 });
