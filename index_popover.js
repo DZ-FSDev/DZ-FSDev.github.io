fceebbd99feaca8daacef4ce30bbb9351071743f
DZ-FSDev <dz.uofm@gmail.com>
2022-04-06 10:57:08 -0500

    rename popover.js

--- index_popover.js
+++ index_popover.js
@@ -1,10 +1,10 @@
-var popover = new bootstrap.Popover(document.querySelector('#popover-dismiss-2021'), {
+new bootstrap.Popover(document.querySelector('#popover-dismiss-2021'), {
     container: 'body',
     trigger: 'focus',
     customClass: 'bg-smoke-success text-success'
 });
 
-var popover2 = new bootstrap.Popover(document.querySelector('#popover-dismiss-2022'), {
+new bootstrap.Popover(document.querySelector('#popover-dismiss-2022'), {
     container: 'body',
     trigger: 'focus',
     customClass: 'bg-smoke-success text-success'
