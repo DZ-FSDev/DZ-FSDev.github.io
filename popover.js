a0d318dd0e218fa52a521cb4a7293b1eea5474e2
DZ-FSDev <dz.uofm@gmail.com>
2022-04-06 08:54:20 -0500

    Update popover.js

--- popover.js
+++ popover.js
@@ -1,4 +1,11 @@
-var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+var popover = new bootstrap.Popover(document.querySelector('#popover-dismiss-2021'), {
     container: 'body',
-    trigger: 'focus'
+    trigger: 'focus',
+    customClass: 'bg-smoke-success text-success'
+});
+
+var popover2 = new bootstrap.Popover(document.querySelector('#popover-dismiss-2022'), {
+    container: 'body',
+    trigger: 'focus',
+    customClass: 'bg-smoke-success text-success'
 });
