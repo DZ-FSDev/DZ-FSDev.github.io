841c944718acdce6dcad173edf3f43fa425551fd
DZ-FSDev <dz.uofm@gmail.com>
2022-04-13 09:09:59 -0500

    Update neoHex.js

--- neoHex.js
+++ neoHex.js
@@ -1,4 +1,4 @@
-/*  Modification Licensing Copyright
+/*  Modifications Licensing Copyright
  * 
  *  Copyright (C) 2022  DZ-FSDev
  *  
@@ -16,8 +16,30 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+/*
+Original Copyright (c) 2022 by Matei Copot
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 /**
- * @author https://codepen.io/towc
+ * @author Matei Copot
  * @author DZ-FSDev
  */
 var w = c.width = window.innerWidth,
@@ -27,26 +49,26 @@ var w = c.width = window.innerWidth,
     opts = {
 
         len: 50,
-        count: 50,
+        count: 70,
         baseTime: 10,
         addedTime: 10,
-        dieChance: .05,
+        dieChance: .035,
         spawnChance: 1,
         sparkChance: .1,
         sparkDist: 10,
-        sparkSize: 2,
+        sparkSize: 3,
 
         color: 'hsl(hue,100%,light%)',
-        baseLight: 25,
-        addedLight: 10, // [15-10,15+10]
+        baseLight: 35,
+        addedLight: 20, // [35-20,35+20]
         shadowToTimePropMult: 6,
         baseLightInputMultiplier: .01,
         addedLightInputMultiplier: .02,
 
-        cx: w  / 2,
+        cx: w / 2,
         cy: h / 2,
-        repaintAlpha: .04,
-        hueChange: .1
+        repaintAlpha: .05,
+        hueChange: .05
     },
 
     tick = 0,
@@ -56,6 +78,11 @@ var w = c.width = window.innerWidth,
 
     baseRad = Math.PI * 2 / 6;
 
+var stop = false,
+    frameCount = 0,
+    $results = $("#results"),
+    fps, fpsInterval, startTime, now, then, elapsed;
+
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, w, h);
 
@@ -64,17 +91,22 @@ function loop() {
     window.requestAnimationFrame(loop);
 
     ++tick;
+    now = Date.now();
+    elapsed = now - then;
 
-    ctx.globalCompositeOperation = 'source-over';
-    ctx.shadowBlur = 0;
-    ctx.fillStyle = 'rgba(0,0,0,alp)'.replace('alp', opts.repaintAlpha);
-    ctx.fillRect(0, 0, w, h);
-    ctx.globalCompositeOperation = 'lighter';
+    if (elapsed > fpsInterval) {
+        then = now - (elapsed % fpsInterval);
+        ctx.globalCompositeOperation = 'source-over';
+        ctx.shadowBlur = 0;
+        ctx.fillStyle = 'rgba(0,0,0,alp)'.replace('alp', opts.repaintAlpha);
+        ctx.fillRect(0, 0, w, h);
+        ctx.globalCompositeOperation = 'lighter';
 
-    if (lines.length < opts.count && Math.random() < opts.spawnChance)
-        lines.push(new Line);
+        if (lines.length < opts.count && Math.random() < opts.spawnChance)
+            lines.push(new Line);
 
-    lines.map(function (line) { line.step(); });
+        lines.map(function (line) { line.step(); });
+    }
 }
 function Line() {
 
@@ -131,7 +163,13 @@ Line.prototype.step = function () {
     if (Math.random() < opts.sparkChance)
         ctx.fillRect(opts.cx + (this.x + x) * opts.len + Math.random() * opts.sparkDist * (Math.random() < .5 ? 1 : -1) - opts.sparkSize / 2, opts.cy + (this.y + y) * opts.len + Math.random() * opts.sparkDist * (Math.random() < .5 ? 1 : -1) - opts.sparkSize / 2, opts.sparkSize, opts.sparkSize)
 }
-loop();
+function initLoop(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    loop();
+}
+initLoop(30);
 
 window.addEventListener('resize', function () {
 
